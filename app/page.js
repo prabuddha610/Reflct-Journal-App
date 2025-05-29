82c0fe2b13e28823b7653c6388ee9319242987d6
prabuddha610 <prabuddha610@gmail.com>
2025-05-30 01:00:31 +0530

    Add Title to Landing Page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      Hello
-      <br/>
-      <Button variant="journal">Button</Button>
+    <div className="relative container mx-auto px-4 pt-16 pb-16">
+      <div className="max-w-5xl mx-auto text-center space-y-8">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl mb-6 gradient-title">Your Space to Reflct. <br/>Your Story to Tell.</h1>
+        <p className="text-lg md:text-xl text-orange-800 mb-8">Capture your thoughts, track your moods, and reflect on your journey in a beautiful, secure space.</p>
+      </div>
     </div>
   );
 }
