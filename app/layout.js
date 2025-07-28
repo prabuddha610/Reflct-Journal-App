64c1e9e52cab3fb010e7fbb728658dafd7bb8837
prabuddha610 <prabuddha610@gmail.com>
2025-07-28 20:03:58 +0530

    Add Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Reflct",
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className}`}>
         <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
         <main className="min-h-screen">{children}</main>
-        
+
         <footer className="bg-orange-300 py-12 bg-opacity-10">
           <div className="container mx-auto px-4 text-center text-gray-900">
             <p>Made with 💗 by Reflct</p>
