ee8a656c52583bdbe0df2c9efeccae57c0f44a67
prabuddha610 <prabuddha610@gmail.com>
2025-07-28 23:26:59 +0530

    Add Clerk User Authentication

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,18 +12,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
-        <Header/>
-        <main className="min-h-screen">{children}</main>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
+          <Header />
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-orange-300 py-12 bg-opacity-10">
-          <div className="container mx-auto px-4 text-center text-gray-900">
-            <p>Made with 💗 by Reflct</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-orange-300 py-12 bg-opacity-10">
+            <div className="container mx-auto px-4 text-center text-gray-900">
+              <p>Made with 💗 by Reflct</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
@@ -16,8 +17,18 @@ const Header = () => {
           />
         </Link>
 
-        <div className='flex items-center gap-4'>
-            {/* Login CTAs */}
+        <div className="flex items-center gap-4">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </nav>
     </header>
