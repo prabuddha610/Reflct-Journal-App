fa8026fa54792efb24c5e1d3c387bde864e1eeec
prabuddha610 <prabuddha610@gmail.com>
2025-07-28 23:04:59 +0530

    Add Header Component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
+        <Header/>
         <main className="min-h-screen">{children}</main>
 
         <footer className="bg-orange-300 py-12 bg-opacity-10">
